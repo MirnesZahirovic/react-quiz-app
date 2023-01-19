4dc73ff81d1645d9ccbd8a55a9b601cf9f865acb
MirnesZahirovic <contact@mirneszahirovic.me>
2023-01-19 13:01:59 +0100

    Deleting all files and starting again

--- src/components/Question.jsx
+++
@@ -1,7 +0,0 @@
-import classes from "./Question.module.css";
-
-function Question(props) {
-  return <div className={classes.question}>{props.question.question}</div>;
-}
-
-export default Question;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import questionsStore from "./store/Questions";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Provider store={questionsStore}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
-
